3eb8e48193a9ba24c9508336bc739d43f5a4ab0b
Jacky Lau <jackylqlong@gmail.com>
2025-10-05 18:26:59 +0800

    update Cinema validation rule with NaN, update unit test for cinema model

--- src/models/Cinema.ts
+++ src/models/Cinema.ts
@@ -14,8 +14,10 @@ export class Cinema {
     // seatMapping: (string)[][]
   ) {
     if (!title) throw new Error("Title is required");
-    if (rows < 1 || rows > 26) throw new Error("Rows must be between 1 and 26");
-    if (seatsPerRow < 1 || seatsPerRow > 50)
+    if (!rows || rows < 1 || rows > 26) {
+      throw new Error("Rows must be between 1 and 26");
+    }
+    if (!seatsPerRow || seatsPerRow < 1 || seatsPerRow > 50)
       throw new Error("Seats per row must be between 1 and 50");
 
     this.id = crypto.randomUUID();
--- tests/models/Cinema.test.ts
+++ tests/models/Cinema.test.ts
@@ -3,6 +3,7 @@ import { Cinema } from "../../src/models/Cinema";
 describe("Cinema Model", () => {
   test("validation rules should reject empty title and invalid rows (< 1 or >26) and seats-per-row (< 1 or >50)", () => {
     expect(() => new Cinema("", 8, 10)).toThrow();
+    expect(() => new Cinema("Movie", NaN, NaN)).toThrow();
     expect(() => new Cinema("Movie", 5, 0)).toThrow();
     expect(() => new Cinema("Movie", 5, 51)).toThrow();
     expect(() => new Cinema("Movie", 0, 50)).toThrow();
